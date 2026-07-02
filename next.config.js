cea026d4d7e14b6ff3072696e93843420d98a032
m2o2a <popmode33@gmail.com>
2026-07-02 07:15:00 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' }
+    ]
+  }
 };
 
 module.exports = nextConfig;
